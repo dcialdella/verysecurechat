7caa567b939d149b5e1a502744809988ac3e2692
dcialdella <dcialdella@gmail.com>
2022-09-22 23:07:08 +0200

    Update c.py

--- c.py
+++ c.py
@@ -90,7 +90,7 @@ def receive_message_from_server(sck, m):
         if len(texts) < 1:
             tkDisplay.insert(tk.END, from_server)
         else:
-            comando='echo ' + from_server + '| gpg -u ' + userid + ' -e -a --no-comment --no-verbose -r 182DA782'
+            comando='echo "' + from_server + '" | gpg -u ' + userid + ' -e -a --no-comment --no-verbose -r 182DA782'
             p = subprocess.run(comando, shell=True, timeout=2, check=True, capture_output=True, text=True)
 #            print(p.stdout )
 
@@ -125,9 +125,9 @@ def getChatMessage(msg):
     # why? Apparently, tkinter does not allow use insert into a disabled Text widget :(
     tkDisplay.config(state=tk.NORMAL)
     if len(texts) < 1:
-        tkDisplay.insert(tk.END, "Tu->" + msg, "tag_your_message") # no line
+        tkDisplay.insert(tk.END, "ENTER->" + msg, "tag_your_message") # no line
     else:
-        tkDisplay.insert(tk.END, "\n\n" + "Tu->" + msg, "tag_your_message")
+        tkDisplay.insert(tk.END, "\n" + "Tu->" + msg, "tag_your_message")
         # aca invoco al S.O.
 
 
@@ -142,6 +142,8 @@ def getChatMessage(msg):
 
 def send_msg_to_server(msg):
     client_msg = str(msg)
+
+#    msg = msg.replace('\n', '')
 
     print( client_msg )
     client.send(client_msg.encode())
